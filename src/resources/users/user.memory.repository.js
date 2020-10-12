926181ce7e68d11c661ebe7d9478b6d47a998917
Egor Piskunov <egor.piskunou@gmail.com>
2020-10-12 19:30:47 +0300

    feat: add user in memory repository

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,37 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+const DB = require('../../common/db/users');
+
+const getAll = async () => await DB.getAllUsers();
+
+const getById = async id => {
+  const user = await DB.getUser(id);
+
+  if (!user) throw new Error(`The user with id ${id} was not found`);
+
+  return user;
 };
 
-module.exports = { getAll };
+const create = async user => {
+  const newUser = await DB.createUser(user);
+
+  if (!newUser) throw new Error(`Could not create user with id ${user.id}`);
+
+  return newUser;
+};
+
+const update = async (id, data) => {
+  const user = await DB.updateUser(id, data);
+
+  if (!user) throw new Error('Could not update user with id ${id}');
+
+  return user;
+};
+
+const remove = async id => {
+  const user = await DB.removeUser(id);
+
+  if (!user) throw new Error('Could not remove user with id ${id}');
+
+  return user;
+};
+
+module.exports = { getAll, getById, create, update, remove };
