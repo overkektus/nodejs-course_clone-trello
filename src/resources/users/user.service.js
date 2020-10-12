28deb25e30518f5ecbe79e3ea8eef9c3f446eb35
Egor Piskunov <egor.piskunou@gmail.com>
2020-10-12 19:31:09 +0300

    feat: add user service repository

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,19 @@
 const usersRepo = require('./user.memory.repository');
+const tasksRepo = require('../boards/tasks/tasks.memory.repository');
 
-const getAll = () => usersRepo.getAll();
+const getAll = async () => await usersRepo.getAll();
 
-module.exports = { getAll };
+const getById = async id => await usersRepo.getById(id);
+
+const create = async user => await usersRepo.create(user);
+
+const update = async (id, user) => await usersRepo.update(id, user);
+
+const remove = async id => {
+  const user = await usersRepo.remove(id);
+  await tasksRepo.updateTasksUser(user.id);
+
+  return user;
+};
+
+module.exports = { getAll, getById, create, update, remove };
