bbad7351a0d737050af90489a9b9ca6f629e73a7
Egor Piskunov <egor.piskunou@gmail.com>
2020-10-12 19:31:32 +0300

    feat: add user routes

--- src/resources/users/user.router.js
+++ src/resources/users/user.router.js
@@ -8,4 +8,52 @@ router.route('/').get(async (req, res) => {
   res.json(users.map(User.toResponse));
 });
 
+router.route('/:id').get(async (req, res) => {
+  try {
+    const {
+      params: { id }
+    } = req;
+    const user = await usersService.getById(id);
+    return res.status(200).send(user);
+  } catch (error) {
+    return res.status(404).send(error.message);
+  }
+});
+
+router.route('/').post(async (req, res) => {
+  try {
+    const { body } = req;
+    const user = await usersService.create(new User({ ...body }));
+    return res.status(200).send(user);
+  } catch (error) {
+    return res.status(404).send(error.message);
+  }
+});
+
+router.route('/:id').put(async (req, res) => {
+  try {
+    const {
+      body,
+      params: { id }
+    } = req;
+    const user = await usersService.update(id, body);
+    return res.status(200).send(user);
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
+});
+
+router.route('/:id').delete(async (req, res) => {
+  try {
+    const {
+      params: { id }
+    } = req;
+    const user = await usersService.remove(id);
+
+    return res.status(204).send(user);
+  } catch (error) {
+    return res.status(404).send(error.message);
+  }
+});
+
 module.exports = router;
