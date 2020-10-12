253bf37514686e72e9b09fbc8adc97e41c907b3c
Egor Piskunov <egor.piskunou@gmail.com>
2020-10-12 19:42:35 +0300

    refact: refact board router

--- src/resources/boards/board.router.js
+++ src/resources/boards/board.router.js
@@ -14,42 +14,56 @@ router.route('/').get(async (req, res) => {
 
 router.route('/:id').get(async (req, res) => {
   try {
-    const board = await boardsService.getById(req.params.id);
+    const {
+      params: { id }
+    } = req;
+    const board = await boardsService.getById(id);
 
     return res.status(200).send(board);
   } catch (error) {
-    return res.status(404).send(error.message);
+    const { message } = error;
+    return res.status(404).send(message);
   }
 });
 
 router.route('/').post(async (req, res) => {
   try {
-    const { title, columns } = req.body;
-    const board = await boardsService.create(new Board({ title, columns }));
+    const { body } = req;
+    const board = await boardsService.create(new Board({ ...body }));
 
     return res.status(200).send(board);
   } catch (error) {
-    return res.status(400).send(error.message);
+    const { message } = error;
+    return res.status(400).send(message);
   }
 });
 
 router.route('/:id').put(async (req, res) => {
   try {
-    const board = await boardsService.update(req.params.id, req.body);
+    const {
+      body,
+      params: { id }
+    } = req;
+    const board = await boardsService.update(id, body);
 
     return res.status(200).send(board);
   } catch (error) {
-    return res.status(400).send(error.message);
+    const { message } = error;
+    return res.status(400).send(message);
   }
 });
 
 router.route('/:id').delete(async (req, res) => {
   try {
-    const board = await boardsService.remove(req.params.id);
+    const {
+      params: { id }
+    } = req;
+    const board = await boardsService.remove(id);
 
     return res.status(204).send(board);
   } catch (error) {
-    return res.status(404).send(error.message);
+    const { message } = error;
+    return res.status(404).send(message);
   }
 });
 
